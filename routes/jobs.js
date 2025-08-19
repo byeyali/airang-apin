0e67ce823217cf36b29d9d0dbc17c475ecd2863e
byeyali <byeyali92@gmail.com>
2025-08-20 08:04:05 +0900

    get job list

--- routes/jobs.js
+++ routes/jobs.js
@@ -5,9 +5,14 @@ const jobController = require("../controllers/jobs");
 
 router.post("/", authorization, jobController.createTutorJob);
 router.put("/:id", authorization, jobController.updateTutorJob);
+router.get("/", authorization, jobController.getTutorJobList);
 router.get("/:id", jobController.getTutorJobById);
 router.delete("/:id", authorization, jobController.deleteTutorJob);
 router.post("/:id/category", authorization, jobController.addTutorJobCategory);
-router.delete("/:id/category", authorization, jobController.deleteTutorJobCategory);
+router.delete(
+  "/:id/category",
+  authorization,
+  jobController.deleteTutorJobCategory
+);
 
 module.exports = router;
