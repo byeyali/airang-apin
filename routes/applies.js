976c60ed12a2d22928aa52035dce53d06ece6451
byeyali <byeyali92@gmail.com>
2025-08-25 02:09:25 +0900

    get match list

--- routes/applies.js
+++ routes/applies.js
@@ -4,6 +4,7 @@ const authorization = require("../middlewares/auth"); // auth.js에서 export한
 const applyController = require("../controllers/applies");
 
 router.post("/", authorization, applyController.createJobApply);
+router.get("/match/me", authorization, applyController.getJobApplyMatch);
 router.get("/:jobId/apply", authorization, applyController.getJobApply);
 router.get(
   "/:jobId/apply-message",
@@ -15,7 +16,6 @@ router.put(
   authorization,
   applyController.updateApplyStatus
 );
-router.get("/match/me", authorization, applyController.getJobApplyMatch);
 router.put("/:id", authorization, applyController.updateJobApply);
 router.post("/:id", authorization, applyController.createContract);
 
